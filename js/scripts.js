b2699871558fe620467b2fa8c47aefa9bdfb0647
ronald_kasendwa <kasendwa@awamo.com>
2019-03-16 13:12:09 +0300

    Implements widgets, implements content section

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(function() {
-	$('video').on('contextmenu', function() {
-		return false;
-	});
+	var mainHeader = $('.main-header'),
+		clockTime = $('.clock-time'),
+		videoElement = $('video').on('contextmenu', function() {
+			return false;
+		})[0];
+	$(window).on('scroll', function() {
+		if (this.scrollY + mainHeader[0].offsetHeight >= videoElement.offsetHeight) {
+			mainHeader.addClass('opaque');
+		} else {
+			mainHeader.removeClass('opaque');
+		}
+	}).trigger('scroll');
+	setTimeout(function() {
+		var currentDateTime = new Date();
+		clockTime.text(currentDateTime.toString("hh:mm") + ' ' + (currentDateTime.getHours() >= 12 ? "PM" : "AM"));
+	}, 1000);
 });
