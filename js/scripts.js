3124c76df6dac97f0c793829b34bcf28adae0995
ronald_kasendwa <kasendwa@awamo.com>
2019-03-18 18:47:50 +0300

    Implements dynamic weather updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(function() {
 	var mainHeader = $('.main-header'),
 		clockTime = $('.widget.clock'),
+		temperature = $('.widget.temperature'),
 		videoElement = $('video').on('contextmenu', function() {
 			return false;
 		})[0];
@@ -11,8 +12,20 @@ $(function() {
 			mainHeader.removeClass('opaque');
 		}
 	}).trigger('scroll');
+	var displayTime;
 	setInterval(function() {
 		var currentDateTime = new Date();
-		clockTime.text(currentDateTime.toString("hh:mm") + ' ' + (currentDateTime.getHours() >= 12 ? "PM" : "AM"));
+		currentDateTime = currentDateTime.toString("hh:mm") + ' ' + (currentDateTime.getHours() >= 12 ? "PM" : "AM");
+		if (displayTime !== currentDateTime) {
+			displayTime = currentDateTime;
+			clockTime.text(currentDateTime);
+		}
 	}, 1000);
+	var updateWeather = function() {
+		$.get("https://api.openweathermap.org/data/2.5/weather?q=Kampala,ug&appid=6ca744f52c9249f074c38af65f845302&units=metric", function(weather) {
+			temperature.html(weather.main.temp + "&#8451;").css('background-image', 'url(https://www.openweathermap.org/img/w/' + weather.weather[0].icon + '.png)');
+		}, "jsonp");
+		setTimeout(updateWeather, 1000 * 60);
+	}
+	updateWeather();
 });
